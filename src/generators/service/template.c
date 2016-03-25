773b27cba2e465281d277acab0bb06d1785154a3
Krisztian Litkey <kli@iki.fi>
2016-03-25 20:51:12 +0200

    service-generator: truncation and concatenation template functions.
    Register template functions for concatenation and truncation. These
    can be used to construct container interface names for iptables
    rules.

--- src/generators/service/template.c
+++ src/generators/service/template.c
@@ -29,9 +29,41 @@
 
 #include "generator.h"
 
+#define FN_TRUNCATE "truncate"
+#define FN_CONCAT   "concat"
+
+static int fn_truncate(smpl_t *smpl, int argc, smpl_value_t *argv,
+                       smpl_value_t *rv, void *user_data);
+
+static int fn_concat(smpl_t *smpl, int argc, smpl_value_t *argv,
+                     smpl_value_t *rv, void *user_data);
+
+static void register_functions(void)
+{
+    static int registered = 0;
+
+    if (registered)
+        return;
+
+    if (smpl_register_function(FN_TRUNCATE, fn_truncate, NULL) < 0) {
+        log_error("Failed to register template function '%s'", FN_TRUNCATE);
+        exit(1);
+    }
+
+    if (smpl_register_function(FN_CONCAT, fn_concat, NULL) < 0) {
+        log_error("Failed to register template function '%s'", FN_CONCAT);
+        exit(1);
+    }
+
+    registered = 1;
+}
+
+
 int template_load(generator_t *g)
 {
     char **errors;
+
+    register_functions();
 
     g->template = smpl_load_template(g->path_template, &errors);
 
@@ -99,3 +131,127 @@ int template_eval(service_t *s)
 }
 
 
+static int fn_truncate(smpl_t *smpl, int argc, smpl_value_t *argv,
+                       smpl_value_t *rv, void *user_data)
+{
+    int32_t  lim, len;
+    char    *str;
+    char    *result;
+
+    IOT_UNUSED(user_data);
+
+    if (rv == NULL)
+        goto invalid_retval;
+
+    if (argc != 2)
+        goto invalid_argc;
+
+    if (argv[0].type != SMPL_VALUE_STRING || argv[1].type != SMPL_VALUE_INTEGER)
+        goto invalid_argv;
+
+    str = argv[0].str;
+    len = strlen(str);
+    lim = argv[1].i32;
+
+    if (lim > len)
+        lim = len;
+
+    result = smpl_allocz(lim + 1);
+
+    if (result == NULL)
+        goto nomem;
+
+    strncpy(result, str, lim);
+    rv->type    = SMPL_VALUE_STRING;
+    rv->str     = result;
+    rv->dynamic = 1;
+
+    return 0;
+
+ invalid_retval:
+    smpl_fail(-1, smpl, EINVAL,
+              "%s() called without a return value", FN_TRUNCATE);
+
+ invalid_argc:
+    rv->type = SMPL_VALUE_UNKNOWN;
+    smpl_fail(-1, smpl, EINVAL,
+              "%s() expects %d arguments, %d given", FN_TRUNCATE, 2, argc);
+
+ invalid_argv:
+    rv->type = SMPL_VALUE_UNKNOWN;
+    smpl_fail(-1, smpl, EINVAL,
+              "%s() expects a string and an integer argument", FN_TRUNCATE);
+
+ nomem:
+    rv->type = SMPL_VALUE_UNKNOWN;
+    return -1;
+}
+
+
+static int fn_concat(smpl_t *smpl, int argc, smpl_value_t *argv,
+                     smpl_value_t *rv, void *user_data)
+{
+    smpl_value_t *arg;
+    char         *p, buf[4096];
+    int           l, n, i;
+
+    IOT_UNUSED(user_data);
+
+    if (rv == NULL)
+        goto invalid_retval;
+
+    p = buf;
+    l = sizeof(buf) - 1;
+
+    for (i = 0, arg = argv; i < argc; i++, arg++) {
+        switch (arg->type) {
+        case SMPL_VALUE_STRING:
+            n = snprintf(p, l, "%s", arg->str);
+            break;
+        case SMPL_VALUE_INTEGER:
+            n = snprintf(p, l, "%d", arg->i32);
+            break;
+        case SMPL_VALUE_DOUBLE:
+            n = snprintf(p, l, "%f", arg->dbl);
+            break;
+        case SMPL_VALUE_UNSET:
+            n = 0;
+            break;
+        default:
+            goto invalid_arg;
+        }
+
+        if (n >= l)
+            goto overflow;
+
+        p += n;
+        l -= n;
+    }
+
+    *p = '\0';
+    rv->type    = SMPL_VALUE_STRING;
+    rv->str     = smpl_strdup(buf);
+    rv->dynamic = 1;
+
+    if (rv->str == NULL)
+        goto nomem;
+
+    return 0;
+
+ invalid_retval:
+    smpl_fail(-1, smpl, EINVAL,
+              "%s() called without a return value", FN_CONCAT);
+
+ invalid_arg:
+    rv->type = SMPL_VALUE_UNKNOWN;
+    smpl_fail(-1, smpl, EINVAL,
+              "%s() expects string, integer, or double arguments", FN_CONCAT);
+
+ overflow:
+    rv->type = SMPL_VALUE_UNKNOWN;
+    smpl_fail(-1, smpl, ENOBUFS, "%s() run out of buffer space", FN_CONCAT);
+
+ nomem:
+    rv->type = SMPL_VALUE_UNKNOWN;
+    return -1;
+}
