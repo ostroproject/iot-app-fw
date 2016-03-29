935e0b2330c0cb7802530bc8ab180577eebc85b1
Krisztian Litkey <krisztian.litkey@intel.com>
2016-03-29 12:40:51 +0300

    repository: removed a lot of old unused cruft.

--- src/package-manager/options.h
+++
@@ -1,9 +0,0 @@
-#ifndef __IOTPM_OPTIONS_H__
-#define __IOTPM_OPTIONS_H__
-
-#include "iotpm.h"
-
-bool iotpm_options_init(iotpm_t *iotpm, int argc, char **argv);
-void iotpm_options_exit(iotpm_t *iotpm);
-
-#endif	/* __IOTPM_OPTIONS_H__ */
